677df03adeae30b78b0962d434845cf246600529
Daniel Diaz <daniel.diaz201355@hotmail.com>
2022-06-27 20:03:12 -0400

    working on login page

--- Frontend/src/pages/Auth/Login/login.jsx
+++ Frontend/src/pages/Auth/Login/login.jsx
@@ -1,10 +1,19 @@
-import { useState } from "react";
+import { useRef,useState,useEffect } from "react";
 import styles from  "./login.module.css";
+import Pathway_Logo from "@/assets/Pathway_Logo.png"
+import loginbackground from "@/assets/login-background.svg"
 
 function Login() {
 
   const [flag, setFlag] = useState(false);
-
+  const myStyle={
+    backgroundImage: `url(${loginbackground})` ,
+    position: 'fixed',
+    minWidth:'100%',
+    minHeight:'100%',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  };
   const toggleState = () => {
     setFlag((prev) => !prev);
     console.log('Flag:',  flag);
@@ -13,12 +22,18 @@ function Login() {
 
   return (
     <>
-    <button onClick={toggleState}>
+    {/* <button onClick={toggleState}>
       Click Me
     </button>
       {
         flag ? "Signup" : "Login"
-      }
+      }*/}
+      <div style={myStyle} className="loginBackground">
+        <div>
+          <img className={styles.pathwayLogo} src= {Pathway_Logo} alt="Logo" />
+        </div>
+      </div>
+      
     </>
   )
 }
