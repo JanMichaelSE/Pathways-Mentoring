b835fbcb23537a5e764625dcac0c82945a80d0c9
JanMichaelSe <janm.montalvo@gmail.com>
2022-07-03 00:39:06 -0400

    Merged with Mike's Changes

--- Frontend/src/pages/Auth/Login/login.jsx
+++ Frontend/src/pages/Auth/Login/login.jsx
@@ -1,19 +1,17 @@
-import { useRef,useState,useEffect } from "react";
-import styles from  "./login.module.css";
-import Pathway_Logo from "@/assets/Pathway_Logo.png"
+import { useRef, useState, useEffect } from "react";
+import styles from "./login.module.css";
+import Pathway_Logo from "@/assets/Pathway_Logo.png";
 import { Link } from "react-router-dom";
 
-
 function Login() {
-
-  const initialValues = { email: "", password: ""};
+  const initialValues = { email: "", password: "" };
   const [formValues, setFormValues] = useState(initialValues);
   const [formErrors, setFormErrors] = useState({});
-  const [ isSubmit, setIsSubmit] = useState(false);
+  const [isSubmit, setIsSubmit] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormValues({ ...formValues, [name]: value});
+    setFormValues({ ...formValues, [name]: value });
     console.log(formValues);
   };
 
@@ -32,20 +30,20 @@ function Login() {
   const validate = (values) => {
     const errors = {};
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
-    if(!values.email){
+    if (!values.email) {
       errors.email = "Email is required!";
-    }else if (!regex.test(values.email)) {
+    } else if (!regex.test(values.email)) {
       errors.email = "This is not a valid email format!";
     }
-    if(!values.password) {
+    if (!values.password) {
       errors.password = "Password is required!";
-    }else if (values.password.length < 12){
+    } else if (values.password.length < 12) {
       errors.password = "Password most be more than 8 characters";
     }
     return errors;
   };
 
- /* const userRef = useRef();
+  /* const userRef = useRef();
   const errRef = useRef();
 
   const [user, setUser] = useState('');
@@ -68,10 +66,9 @@ function Login() {
   //  console.log('Flag:',  flag);
   //}
 
-
   return (
     <>
-    {/* <button onClick={toggleState}>
+      {/* <button onClick={toggleState}>
       Click Me
     </button>
       {
@@ -79,7 +76,7 @@ function Login() {
       }*/}
       <div className={styles.loginBackground}>
         <div className={styles.logoContainer}>
-          <img className={styles.pathwayLogo} src= {Pathway_Logo} alt="Logo" />
+          <img className={styles.pathwayLogo} src={Pathway_Logo} alt="Logo" />
         </div>
         <div className={styles.formContainer}>
           {/*{Object.keys(formErrors).length === 0 && isSubmit ? (<div className="ui message success">Signed successfully</div>
@@ -87,46 +84,48 @@ function Login() {
           <pre>{JSON.stringify(formValues,undefined,2)}</pre>
           )}*/}
           <form onSubmit={handleSubmit}>
-                <input
-                      className={styles.inputUser}
-                      type="email"
-                      name="email"
-                      id="email"
-                      autoComplete="off"
-                      onChange={handleChange} /*{(e) => setUser(e.target.value)}*/
-                      value={formValues.email}
-                      placeholder="Email"
-                />
-                <p>{ formErrors.email}</p>
-                <input
-                      className={styles.inputPwd}
-                      type="password"
-                      name="password"
-                      id="password"
-                      onChange={handleChange} /*{(e) => setPwd(e.target.value)}*/
-                      value={formValues.password}
-                      placeholder="Password"
-                />
-                <p>{ formErrors.password}</p>
-                <button className={styles.button}>Log In</button>
+            <input
+              className={styles.inputUser}
+              type="email"
+              name="email"
+              id="email"
+              autoComplete="off"
+              onChange={handleChange} /*{(e) => setUser(e.target.value)}*/
+              value={formValues.email}
+              placeholder="Email"
+            />
+            <p>{formErrors.email}</p>
+            <input
+              className={styles.inputPwd}
+              type="password"
+              name="password"
+              id="password"
+              onChange={handleChange} /*{(e) => setPwd(e.target.value)}*/
+              value={formValues.password}
+              placeholder="Password"
+            />
+            <p>{formErrors.password}</p>
+            <button className={styles.button}>Log In</button>
           </form>
         </div>
         <div className={styles.linkContainer}>
           <h2 className={styles.subHeading}>
-              New to Pathways?{" "}
-              <Link to={"/"} className={styles.navigationLink}>
-                Create an account.
-              </Link><br />
-           </h2>
-           <h2  className={styles.subHeading}>
-              <Link to={"/"} className={styles.navigationLink}>
-                Forgot password?
-              </Link><br />
-            </h2>
+            New to Pathways?{" "}
+            <Link to={"/"} className={styles.navigationLink}>
+              Create an account.
+            </Link>
+            <br />
+          </h2>
+          <h2 className={styles.subHeading}>
+            <Link to={"/"} className={styles.navigationLink}>
+              Forgot password?
+            </Link>
+            <br />
+          </h2>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Login
+export default Login;
