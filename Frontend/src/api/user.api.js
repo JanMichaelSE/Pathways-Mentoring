62e70c038b18f691ace064defb5e553362817616
JanMichaelSe <janm.montalvo@gmail.com>
2022-07-07 08:01:55 -0400

    Added Mentors Form to Signup and Connected it with the backend endpoint

--- Frontend/src/api/user.api.js
+++ Frontend/src/api/user.api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-async function httpLogin(userParam) {
+async function httpLogin(user) {
   let userToReturn = {
     hasError: false,
     data: null,
@@ -9,8 +9,8 @@ async function httpLogin(userParam) {
 
   try {
     const userInfo = {
-      email: userParam.email,
-      password: userParam.password,
+      email: user.email,
+      password: user.password,
     };
 
     const response = await axios.post(
@@ -19,7 +19,6 @@ async function httpLogin(userParam) {
     );
 
     userToReturn.data = response.data;
-    console.log(userToReturn);
   } catch (error) {
     const errorResponse = error.response.data;
     if (errorResponse.error.errorCode == 401) {
@@ -34,7 +33,7 @@ async function httpLogin(userParam) {
   return userToReturn;
 }
 
-async function httpSignupStudent(studentParam) {
+async function httpSignupStudent(student) {
   let userToReturn = {
     hasError: false,
     data: null,
@@ -42,18 +41,18 @@ async function httpSignupStudent(studentParam) {
   };
 
   try {
-    const name = `${studentParam.firstName} ${studentParam.lastName}`;
+    const name = `${student.firstName} ${student.lastName}`;
 
     const studentInfo = {
       name: name,
-      password: studentParam.password,
-      email: studentParam.email,
+      password: student.password,
+      email: student.email,
       role: "student",
-      phone: studentParam.phone,
-      gender: studentParam.gender,
-      gpa: Number(studentParam.gpa),
-      institution: studentParam.institution,
-      fieldOfStudy: studentParam.fieldOfStudy,
+      phone: student.phone,
+      gender: student.gender,
+      gpa: Number(student.gpa),
+      institution: student.institution,
+      fieldOfStudy: student.fieldOfStudy,
     };
 
     const response = await axios.post(
@@ -76,4 +75,47 @@ async function httpSignupStudent(studentParam) {
   return userToReturn;
 }
 
-export { httpSignupStudent, httpLogin };
+async function httpSignupMentor(mentor) {
+  let userToReturn = {
+    hasError: false,
+    data: null,
+    errorMessage: "",
+  };
+
+  try {
+    const name = `${mentor.firstName} ${mentor.lastName}`;
+
+    const mentorInfo = {
+      name: name,
+      password: mentor.password,
+      email: mentor.email,
+      role: "mentor",
+      phone: mentor.phone,
+      gender: mentor.gender,
+      academicDegree: mentor.academicDegree,
+      department: mentor.department,
+      interests: mentor.interests,
+      facultyStatus: mentor.facultyStatus,
+    };
+
+    const response = await axios.post(
+      "http://localhost:5000/auth/signup/mentor",
+      mentorInfo
+    );
+
+    userToReturn.data = response.data;
+  } catch (error) {
+    const errorResponse = error.response.data;
+    if (errorResponse.error.errorCode == 400) {
+      userToReturn.hasError = true;
+      userToReturn.errorMessage = errorResponse.error.errorMessage;
+    } else if (errorResponse.error.errorCode == 500) {
+      userToReturn.hasError = true;
+      userToReturn.errorMessage = errorResponse.error.errorMessage;
+    }
+  }
+
+  return userToReturn;
+}
+
+export { httpLogin, httpSignupStudent, httpSignupMentor };
---
+++ Frontend/src/components/Auth/MentorForm/mentor-form.jsx
@@ -0,0 +1,140 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import { toast } from "react-toastify";
+
+import { httpSignupMentor } from "@/api/user.api";
+import { useUserStore } from "@/store/user.store";
+
+import Button from "@/components/common/Button/button";
+import Input from "@/components/common/Input/input";
+import Select from "@/components/common/Select/select";
+import styles from "./mentor-form.module.css";
+
+function MentorForm() {
+  const navigate = useNavigate();
+  const userId = useUserStore((state) => state.userId);
+  const setUser = useUserStore((state) => state.setUser);
+
+  // This routing logic will change depending on what the professor
+  // tells us about the application.
+  useEffect(() => {
+    if (userId) {
+      navigate("../mentor", { replace: true });
+    }
+  }, [userId]);
+
+  async function handleSubmit(mentorInfo) {
+    const userResponse = await httpSignupMentor(mentorInfo);
+
+    if (userResponse.hasError) {
+      return toast.error(userResponse.errorMessage);
+    }
+
+    setUser(
+      userResponse.data.userId,
+      userResponse.data.email,
+      userResponse.data.role
+    );
+  }
+
+  return (
+    <div className={styles.formContainer}>
+      <Formik
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          phone: "",
+          email: "",
+          password: "",
+          confirmPassword: "",
+          gender: "",
+          academicDegree: "",
+          department: "",
+          facultyStatus: "",
+          areaOfInterest: "",
+        }}
+        validationSchema={Yup.object({
+          firstName: Yup.string().required("First Name is required"),
+          lastName: Yup.string().required("Last Name is required"),
+          phone: Yup.string()
+            .matches(
+              /^(\+?\d{0,4})?\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{4}\)?)?$/,
+              "Phone number is not valid"
+            )
+            .min(10, "Phone number must be 10 digits")
+            .required("Phone is required"),
+          email: Yup.string()
+            .email("Invalid email address")
+            .required("Email is required"),
+          password: Yup.string()
+            .min(12, "Password must be at least 12 characters")
+            .required("Password is required"),
+          confirmPassword: Yup.string()
+            .oneOf([Yup.ref("password"), null], "Passwords must match")
+            .required("Password Confirmation is required"),
+          gender: Yup.string()
+            .oneOf(["Male", "Female", "Other"])
+            .required("Gender is required"),
+          academicDegree: Yup.string().required("Academic Degree is required"),
+          department: Yup.string().required("Department is required"),
+          areaOfInterest: Yup.string(),
+          facultyStatus: Yup.string()
+            .oneOf(["Instructor", "Assistant", "Associate", "Professor"])
+            .required("Faculty Status is required"),
+        })}
+        onSubmit={async (values) => {
+          await handleSubmit(values);
+        }}
+      >
+        <Form className={styles.formContainer}>
+          <div className={styles.formInput}>
+            <Input label="First Name *" name="firstName" type="text" />
+            <Input label="Last Name *" name="lastName" type="text" />
+            <Select label="Academic Degree *" name="academicDegree">
+              <option value="">Select Option</option>
+              <option value="Master">Master</option>
+              <option value="Doctorate">Doctorate</option>
+              <option value="Post Doctorate">Post Doctorate</option>
+            </Select>
+            <Input label="Phone *" name="phone" type="tel" />
+            <Input label="Email *" name="email" type="text" />
+            <Input label="Password *" name="password" type="password" />
+            <Input
+              label="Confirm Password *"
+              name="confirmPassword"
+              type="password"
+            />
+            <Select label="Department *" name="department">
+              <option value="">Select Option</option>
+              <option value="Computer Science">Computer Science</option>
+              <option value="Doctorate">Computer Enginnering</option>
+              <option value="Post Doctorate">Electrical Engineering</option>
+            </Select>
+            <Input label="Area of Interest" name="areaOfInterest" type="text" />
+            <Select label="Gender *" name="gender">
+              <option value="">Select Option</option>
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+              <option value="Other">Other</option>
+            </Select>
+            <Select label="Faculty Status *" name="facultyStatus">
+              <option value="">Select Option</option>
+              <option value="Instructor">Instructor</option>
+              <option value="Assistant">Assistant</option>
+              <option value="Associate">Associate</option>
+              <option value="Professor">Professor</option>
+            </Select>
+          </div>
+
+          <div className={styles.buttonContainer}>
+            <Button type="submit">Sign Up</Button>
+          </div>
+        </Form>
+      </Formik>
+    </div>
+  );
+}
+
+export default MentorForm;
