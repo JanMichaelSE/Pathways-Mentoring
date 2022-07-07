34e68101c366d74428067b89b22258c0f88ffd50
Luis Telemaco <ltelemaco14@gmail.com>
2022-07-06 23:08:24 -0400

    Developed student navbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,12 @@
-import './styles/App.css'
-import AuthRouter from '@/routes/root.router.jsx';
+import "./styles/App.css";
+import AuthRouter from "@/routes/root.router.jsx";
 
 function App() {
-
   return (
     <div className="App">
       <AuthRouter />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
