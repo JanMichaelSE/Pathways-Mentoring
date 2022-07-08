25ca9e3c22a50671af0492c219b79affc18aa935
Luis Telemaco <ltelemaco14@gmail.com>
2022-07-07 22:00:00 -0400

    Finished styling the Student navbar, and began working on the assessment view page

--- Frontend/src/pages/Students/Assessments/assessments.jsx
+++ Frontend/src/pages/Students/Assessments/assessments.jsx
@@ -1,11 +1,59 @@
+import { useState } from "react";
+
+import Button from "@/components/common/Button/button.jsx";
 import styles from "./assessments.module.css";
 
 function Assessments() {
+  const [sortAscending, setSortAscending] = useState(true);
+
   return (
     <div className={styles.container}>
+      <div className={styles.buttonsContainer}>
+        <Button
+          style={{
+            width: "10vw",
+            height: "8vh",
+            fontSize: "20px",
+            marginRight: "3vw",
+          }}
+        >
+          Filter
+        </Button>
+        <Button style={{ width: "15vw", height: "8vh", fontSize: "20px" }}>
+          Sort Desc
+        </Button>
+      </div>
       <div className={styles.assessmentsContainer}>
         <div className={styles.assessment}>
-          {/*Assessment Title*/}
+          {/*Assessment Title 1*/}
+          {/*Assessment Description*/}
+        </div>
+        <div className={styles.assessment}>
+          {/*Assessment Title 2*/}
+          {/*Assessment Description*/}
+        </div>
+        <div className={styles.assessment}>
+          {/*Assessment Title 3*/}
+          {/*Assessment Description*/}
+        </div>
+        <div className={styles.assessment}>
+          {/*Assessment Title 4*/}
+          {/*Assessment Description*/}
+        </div>
+        <div className={styles.assessment}>
+          {/*Assessment Title 5*/}
+          {/*Assessment Description*/}
+        </div>
+        <div className={styles.assessment}>
+          {/*Assessment Title 6*/}
+          {/*Assessment Description*/}
+        </div>
+        <div className={styles.assessment}>
+          {/*Assessment Title 7*/}
+          {/*Assessment Description*/}
+        </div>
+        <div className={styles.assessment}>
+          {/*Assessment Title 8*/}
           {/*Assessment Description*/}
         </div>
       </div>
