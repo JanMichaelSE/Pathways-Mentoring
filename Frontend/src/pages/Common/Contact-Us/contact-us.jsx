1f402088a2918f1b2bd7bcc3f158a55d5e9c60a1
Javier Nazario <javand1019@gmail.com>
2022-07-25 07:55:40 -0400

    Contact Form 0.1.1

--- Frontend/src/pages/Common/Contact-Us/contact-us.jsx
+++ Frontend/src/pages/Common/Contact-Us/contact-us.jsx
@@ -1,8 +1,70 @@
+import styles from "./contact-us.module.css"
+import Navbar from "../../../components/Students/StudentsNavbar/students-navbar"
+import Input from "../../../components/common/Input/input"
+import { Checkbox } from '@chakra-ui/react'
 
-function ContactUs() {
+export default function ContactUs() {
   return (
-    <div>Contact Us</div>
+    <>
+    <div className={styles.contactUsBackground}>
+    <Navbar />
+    <div className={styles.segmentContainer}>
+      <div className={styles.contactUsForm}>
+          <div className={styles.formSegment}>
+            <img
+            src="/assets/Contact.svg"
+            alt="ContactIcon"
+            className={styles.LogoLink}
+            />
+            <h2 className={styles.SubHeading}>
+              Contact Pathways
+            </h2>
+          </div>
+          <div className={styles.formSegment}>
+            <Input label={"Name *"}/>
+            <Input label={"Topic *"}/>
+          </div>
+          <div className={styles.formSegment}>
+            <Input label={"Email *"}/>
+            <Input label={"Phone Number *"}/>
+          </div>
+          <div className={styles.formSegment}>
+            <Input label={"Message *"}/>
+          </div>
+          <div className={styles.formSegment}>
+            <Checkbox iconColor='grey.400' iconSize='1rem'>
+              I have read and agree to the Privacy Policy of the Polytechnic University of Puerto Rico.
+            </Checkbox>
+          </div>
+      </div>
+      <div className={styles.pathwaysStaff}>
+          <div className={styles.StaffContactBox}>
+            <div>
+              <Avatar size='lg' name='Jan Montalvo' src='assets/Jan.png' />
+            </div>
+            <h3>Jan Montalvo</h3>
+            <h4>CS/COE</h4>
+            <div className={styles.linkBoxes}>
+              <img 
+              src="/assets/Gmail-logo.svg"
+              alt="EmailIcon"
+              className={styles.LogoLink}
+              />
+              <img 
+              src="/assets/Phone-Squared.svg"
+              alt="PhoneIcon"
+              className={styles.LogoLink}
+              />
+              <img 
+              src="/assets/Company.svg"
+              alt="CompanyIcon"
+              className={styles.LogoLink}
+              />
+            </div>
+          </div>
+      </div>
+    </div>
+    </div>
+    </>
   )
 }
-
-export default ContactUs
