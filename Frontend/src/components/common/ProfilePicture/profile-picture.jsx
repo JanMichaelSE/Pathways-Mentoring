a4a3e493b818834b422e3aac0a41c89165e81dc7
Daniel Diaz <daniel.diaz201355@hotmail.com>
2022-08-19 02:15:53 -0400

    Add Border to SVG Avatar on Profile Page

--- Frontend/src/components/common/ProfilePicture/profile-picture.jsx
+++ Frontend/src/components/common/ProfilePicture/profile-picture.jsx
@@ -1,19 +1,14 @@
 import { Image } from "@chakra-ui/react";
 import { useUserStore } from "@/store/user.store";
+import styles from "./profile-picture.module.css";
 
 function ProfilePicture({ ...props }) {
   const pictureData = useUserStore((state) => state.pictureData);
   if (pictureData == "Profile-avatar.svg") {
-    return (
-      <Image
-        borderRadius="full"
-        boxSize="200px"
-        src="/assets/Profile-avatar.svg"
-      ></Image>
-    );
+    return <Image boxSize="200px" src="/assets/Profile-avatar.svg"></Image>;
   } else {
     return (
-      <svg>
+      <svg className={styles.imageContainer}>
         <use href={"/assets/spriteAvatar.svg#" + pictureData}></use>
       </svg>
     );
