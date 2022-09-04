2a7adf1e3e68bcd6a7f605bde3e54110da2d7b44
Daniel Diaz <daniel.diaz201355@hotmail.com>
2022-09-03 22:56:34 -0400

    add folder and file for record view student and mentor, and development plan student

---
+++ Frontend/src/pages/Mentors/Record-View/mentor-record-view.jsx
@@ -0,0 +1,7 @@
+import styles from "./mentor-record-view.module.css";
+
+function RecordView() {
+  return <div>Individual Record</div>;
+}
+
+export default RecordView;
---
+++ Frontend/src/pages/Students/Development-Plan/development-plan.jsx
@@ -0,0 +1,11 @@
+import styles from "./development-plan.module.css";
+
+function DevelopmentPlan() {
+  return (
+    <>
+      <div>Development Plan Page</div>
+    </>
+  );
+}
+
+export default DevelopmentPlan;
