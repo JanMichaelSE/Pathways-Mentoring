dba383cfb8cccd8dc1935213bfa807fac975beea
Daniel Diaz <daniel.diaz201355@hotmail.com>
2022-09-09 16:17:13 -0400

    work on user side of admin page

--- Frontend/src/pages/Admin/Manage-Users.jsx/manage-users.jsx
+++ Frontend/src/pages/Admin/Manage-Users.jsx/manage-users.jsx
@@ -1,8 +1,57 @@
+import React, { Fragment, useState, useEffect } from "react";
+import {
+  HStack,
+  SimpleGrid,
+  Text,
+  Image,
+  Spinner,
+  useToast,
+} from "@chakra-ui/react";
+import styles from "./manage-users.module.css";
+import NoItemsFound from "@/components/common/NoItemsFound/no-items-found";
+import SadFaceIcon from "@/assets/sad-face-icon.svg";
 
 function ManageUsers() {
+  const toast = useToast();
+  const [mentors, setMentors] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  function loadMentorComponent() {
+    if (isLoading) {
+      return (
+        <div style={{ flex: 1, backgroundColor: "#f1f8fc", height: "92vh" }}>
+          <Spinner
+            thickness="5px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="blue.500"
+            size="xl"
+            position="absolute"
+            top="30%"
+            left="50%"
+          />
+        </div>
+      );
+    } else if (mentors.length === 0) {
+      return (
+        <div className={styles.noUsers}>
+          <NoItemsFound title="No Users Pending" icon={SadFaceIcon} />
+        </div>
+      );
+    } else {
+      return (
+        <>
+          <div>Manage Users</div>
+        </>
+      );
+    }
+  }
+
   return (
-    <div>Manage Users</div>
-  )
+    <>
+      <div>{loadMentorComponent()}</div>
+    </>
+  );
 }
 
-export default ManageUsers
+export default ManageUsers;
