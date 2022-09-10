6d3a42719bd117bbe474c8883632c3b9b3d252ca
JanMichaelSe <janm.montalvo@gmail.com>
2022-09-10 10:35:56 -0400

    Finished Get Records By User

--- Backend/src/routes/records/records.controller.ts
+++ Backend/src/routes/records/records.controller.ts
@@ -3,18 +3,55 @@ import { findMentorByUserId } from "../../models/mentors.model";
 import {
   createRecords,
   getAllRecords,
+  getRecordsByMentor,
+  getRecordsByStudent,
   updateRecord,
 } from "../../models/records.model";
 import { findStudentByUserId } from "../../models/students.model";
 import {
   handleBadRequestResponse,
   handleErrorResponse,
+  handleNotFoundResponse,
   isValidUUID,
 } from "../../utils/helpers";
 
 async function httpGetAllRecords(req: Request, res: Response) {
   try {
     const records = await getAllRecords();
+    return res.status(200).json(records);
+  } catch (error) {
+    return handleErrorResponse("get all records", error, res);
+  }
+}
+
+async function httpGetRecordsByStudent(req: Request, res: Response) {
+  try {
+    const studentUserId = req.userId;
+
+    const student = await findStudentByUserId(studentUserId);
+    if (!student) {
+      return handleNotFoundResponse("No student with this access token found.", res);
+    }
+
+    const records = await getRecordsByStudent(student.id);
+
+    return res.status(200).json(records);
+  } catch (error) {
+    return handleErrorResponse("get all records", error, res);
+  }
+}
+
+async function httpGetRecordsByMentor(req: Request, res: Response) {
+  try {
+    const mentorUserId = req.userId;
+
+    const mentor = await findMentorByUserId(mentorUserId);
+    if (!mentor) {
+      return handleNotFoundResponse("No mentor with this access token found.", res);
+    }
+
+    const records = await getRecordsByMentor(mentor.id);
+
     return res.status(200).json(records);
   } catch (error) {
     return handleErrorResponse("get all records", error, res);
@@ -36,10 +73,7 @@ async function httpCreateRecords(req: Request, res: Response) {
     const mentor = await findMentorByUserId(mentorUserId);
     const student = await findStudentByUserId(studentUserId);
     if (!mentor || !student) {
-      return handleBadRequestResponse(
-        "No mentor or student exists with the provided id's.",
-        res
-      );
+      return handleBadRequestResponse("No mentor or student exists with the provided id's.", res);
     }
 
     const records = await createRecords(mentor.id, student.id);
@@ -84,4 +118,10 @@ async function httpDeleteRecord(req: Request, res: Response) {
   }
 }
 
-export { httpGetAllRecords, httpCreateRecords, httpUpdateRecord };
+export {
+  httpGetAllRecords,
+  httpGetRecordsByStudent,
+  httpGetRecordsByMentor,
+  httpCreateRecords,
+  httpUpdateRecord,
+};
