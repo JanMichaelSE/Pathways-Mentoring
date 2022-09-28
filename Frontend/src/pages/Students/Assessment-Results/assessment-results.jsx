71d57b943eac4697dd9abeed3c6f3d36dfdd4aac
JanMichaelSe <janm.montalvo@gmail.com>
2022-09-27 23:22:31 -0400

    Fixed Assessment Results Initial Load

--- Frontend/src/pages/Students/Assessment-Results/assessment-results.jsx
+++ Frontend/src/pages/Students/Assessment-Results/assessment-results.jsx
@@ -1,24 +1,66 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 import { useNavigate } from "react-router-dom";
 import { useAssessmentStore } from "@/store/assessment.store";
+import { Spinner } from "@chakra-ui/react";
 
 import DescriptionCard from "@/components/common/DescriptionCard/description-card";
 import AssessmentResultsGrid from "@/components/Students/AssessmentResultsGrid/assessment-results-grid";
 import Button from "@/components/common/Button/button";
+
+import { httpGetPathwaysAssessment } from "@/api/assessments.api";
 
 import styles from "./assessment-results.module.css";
 
 function AssessmentResults() {
   const navigate = useNavigate();
   const assessment = useAssessmentStore((state) => state.assessment);
+  const setAssessment = useAssessmentStore((state) => state.setAssessment);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     window.scrollTo(0, 0);
+
+    async function loadAssessmentResults() {
+      const assessmentResponse = await httpGetPathwaysAssessment();
+
+      if (assessmentResponse.hasError) {
+        return toast({
+          description: assessmentResponse.errorMessage,
+          status: "error",
+          position: "top",
+          duration: 5000,
+        });
+      }
+
+      setAssessment(assessmentResponse.data);
+      setIsLoading(false);
+    }
+
+    if (assessment == null) {
+      loadAssessmentResults();
+    } else {
+      setIsLoading(false);
+    }
   }, []);
 
   function goBack() {
     navigate("../assessments", { replace: true });
+  }
+
+  if (isLoading) {
+    return (
+      <Spinner
+        thickness="5px"
+        speed="0.65s"
+        emptyColor="gray.200"
+        color="blue.500"
+        size="xl"
+        position="absolute"
+        top="30%"
+        left="50%"
+      />
+    );
   }
 
   return (
