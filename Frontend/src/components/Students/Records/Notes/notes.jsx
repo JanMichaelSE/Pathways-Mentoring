40132e741c497611dfd72cee2b192aebe399b8da
Daniel Diaz <daniel.diaz201355@hotmail.com>
2022-09-29 18:47:55 -0400

    worked on notes adding a message to receiver and sender

--- Frontend/src/components/Students/Records/Notes/notes.jsx
+++ Frontend/src/components/Students/Records/Notes/notes.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import ReceiverMessage from "../ReceiverMessage/receiver-message";
+import SenderMessage from "../SenderMessage/sender-message";
 import styles from "./notes.module.css";
 
 function Notes() {
   return (
     <>
       <div className={styles.notesContainer}>
-        <div className={styles.messageContainer}></div>
+        <div className={styles.messageContainer}>
+          <ReceiverMessage />
+          <SenderMessage />
+        </div>
         <div className={styles.buttonsContainer}>
           <input type="text" placeholder="Message" className={styles.input} />
           <button className={styles.btn}>Send</button>
