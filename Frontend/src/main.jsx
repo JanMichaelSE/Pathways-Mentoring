9bbd78e3e3b0e1652bfc97bc90e7ea4cd19dbf1d
JanMichaelSe <janm.montalvo@gmail.com>
2022-10-07 23:50:24 -0400

    Fixed React Strict Mode and Float issue in Profile Pages

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,9 +5,7 @@ import App from "./App";
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
   <BrowserRouter>
     <App />
   </BrowserRouter>
-  // </React.StrictMode>
 );
